88829acf2a00dabba35976a1f193cf557fbe6027
animsh <animsh.more@gmail.com>
2020-11-28 14:45:30 +0530

    changelog: 1. login, sign up and forget password working

--- app/src/main/java/com/animsh/trace/ui/LoginActivity.java
+++ app/src/main/java/com/animsh/trace/ui/LoginActivity.java
@@ -1,16 +1,152 @@
 package com.animsh.trace.ui;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.animsh.trace.MainActivity;
 import com.animsh.trace.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText getUserEmail;
+    EditText getPassword;
+    ImageView imageLogin;
+    TextView txtSignUpPage;
+    TextView txtForgetPassword;
+    FirebaseAuth firebaseAuth;
+    private ProgressDialog loadingBar;
+    private String parentDBName = "Users";
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        // check activity id already opened
+
+        if (restorePrefData()) {
+            Intent homePageActivity = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(homePageActivity);
+            finish();
+        }
+
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+        getUserEmail = findViewById(R.id.login_username);
+        getPassword = findViewById(R.id.login_password);
+        imageLogin = findViewById(R.id.image_login);
+        txtSignUpPage = findViewById(R.id.txt_sign_up_page);
+        txtForgetPassword = findViewById(R.id.txt_forgot_password_page);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loadingBar = new ProgressDialog(this, R.style.AppCompatAlertDialogStyle);
+        imageLogin.setImageResource(R.drawable.img_sign_in);
+
+        txtSignUpPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        txtForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void loginUser() {
+
+        String password = getPassword.getText().toString();
+        String email = getUserEmail.getText().toString();
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show();
+            if (password.length() < 8) {
+                Toast.makeText(this, "Password length should be 8 characters", Toast.LENGTH_SHORT).show();
+            }
+        } else if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show();
+            if (getUserEmail.getText().toString().matches(emailPattern)) {
+                Toast.makeText(this, "Please enter valid email", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            loadingBar.setTitle("Login You In");
+            loadingBar.setMessage("Please wait while we are checking your details");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            allowAccessToAccount(email, password);
+
+        }
+
+    }
+
+    private void allowAccessToAccount(final String email, final String password) {
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            savePrefsData();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            String error = task.getException().getMessage();
+                            Toast.makeText(LoginActivity.this, "Error: " + error, Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+
+    }
+
+    public boolean restorePrefData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        return pref.getBoolean("isUserLogin", false);
+    }
+
+    private void savePrefsData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isUserLogin", true);
+        editor.apply();
+    }
+
+
 }
