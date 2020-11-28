9b97d8d789155390d05277d1d09998bc507b2b96
animsh <animsh.more@gmail.com>
2020-11-28 12:16:00 +0530

    changelog: 1. connected to the firebases

---
+++ app/src/main/java/com/animsh/trace/ui/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.animsh.trace.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.animsh.trace.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
